a7eaff17a917040a37ece838ae6042d0ced6a756
rupertn <rupertneve28@gmail.com>
2021-03-01 18:09:57 -0700

    added stopword removal function

--- comment_preprocessing.py
+++ comment_preprocessing.py
@@ -2,7 +2,53 @@ import pandas as pd
 import numpy as np
 import re
 import string
+import nltk
+from nltk.corpus import stopwords
 from clean_rosters import nickname_dict
+
+
+def remove_nicknames(name_dict, comm):
+    words = comm.split()
+
+    for word in words:
+        if word in name_dict.keys():
+            regex = r'\b{}\b'.format(word)
+            comm = re.sub(regex, name_dict[word], comm)
+
+    return comm
+
+
+def remove_links(comm):
+    return re.sub(r'[\(\[].*?[\)\]]', '', comm)
+
+
+def remove_numbers(comm):
+    return re.sub(r'\d+', '', comm)
+
+
+def remove_punctuation(comm):
+    return ''.join([char.lower() for char in comm if char not in string.punctuation + '‘’”“'])
+
+
+def remove_whitespace(comm):
+    return re.sub(r'\s+', ' ', comm).strip()
+
+
+def remove_stopwords(stop_list, comm):
+    return ' '.join([word for word in comm.split() if word not in stop_list])
+
+
+def clean_comment(stop_list, name_dict, comm):
+
+    comm = remove_links(comm)
+    comm = remove_numbers(comm)
+    comm = remove_punctuation(comm)
+    comm = remove_whitespace(comm)
+    comm = remove_nicknames(name_dict, comm)
+    comm = remove_stopwords(comm)
+
+    return comm
+
 
 df = pd.read_csv('lakers_game_comments.csv')
 
@@ -14,32 +60,10 @@ df = df.explode('comment_body')
 df = df.replace({'comment_body': ''}, np.nan)
 df = df.dropna(subset=['comment_body'])
 
+nltk.download('stopwords')
+stop_words = stopwords.words('english')
 
-def remove_nicknames(name_dict, comm):
-    words = comm.split()
-
-    for word in words:
-        for key, item in name_dict.items():
-            if word == key:
-                regex = r'\b{}\b'.format(word)
-                comm = re.sub(regex, item, comm)
-
-    return comm
-
-
-def remove_noise(name_dict, comm):
-
-    comm = re.sub(r'[\(\[].*?[\)\]]', '', comm)  # removes all text and brackets inside () or [].
-    # comm = re.sub(r'\d+', '', comm)  # removes numbers
-    comm = ''.join([char.lower() for char in comm if char not in string.punctuation + '‘'])
-    comm = re.sub(r'\s+', ' ', comm).strip()  # removes extra whitespaces
-
-    comm = remove_nicknames(name_dict, comm)
-
-    return comm
-
-
-df['comment_body'] = df['comment_body'].apply(lambda x: remove_noise(nickname_dict, x))
+df['comment_body'] = df['comment_body'].apply(lambda x: clean_comment(stop_words, nickname_dict, x))
 
 # TODO: remove punctuation, links, images from comments etc.
 # TODO: Standardize words.
