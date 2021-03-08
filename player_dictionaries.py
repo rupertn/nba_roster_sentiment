596c7178c8455da57775a62f692793789899c777
rupertn <rupertneve28@gmail.com>
2021-03-07 17:51:03 -0700

    added names to skip when looking for comment-player match

--- player_dictionaries.py
+++ player_dictionaries.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('data/roster_clean.csv')
 
-nicknames = dict(zip(df.initials.str.lower(), df.name.str.lower()))
+nicknames = dict(zip(df.initials, df.name))
 
 nicknames['kuz'] = 'kyle kuzma'
 nicknames['bron'] = 'lebron james'
@@ -20,15 +20,8 @@ del nicknames['js']
 # TODO: deal with JR abbreviation
 # TODO: fix ennis and dennis problem
 
-first_to_full = dict(zip(df.first_name.str.lower(), df.name.str.lower()))
+first_to_full = dict(zip(df.first_name, df.name))
+skip_first = ['talen', 'jordan']
 
-del first_to_full['talen']
-del first_to_full['jordan']
-
-last_to_full = dict(zip(df.last_name.str.lower(), df.name.str.lower()))
-
-del last_to_full['bryant']
-del last_to_full['cook']
-del last_to_full['wear']
-del last_to_full['ball']
-del last_to_full['hart']
+last_to_full = dict(zip(df.last_name, df.name))
+skip_last = ['bryant', 'cook', 'wear', 'ball', 'hart']
