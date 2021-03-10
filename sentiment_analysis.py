c6d19dec5538d917e06ddc2b05d0e9d2faccb552
rupertn <rupertneve28@gmail.com>
2021-03-10 14:57:28 -0700

    renamed variables

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -8,13 +8,16 @@ p = pd.read_csv('data/game_posts_clean.csv')
 r = pd.read_csv('data/roster_train.csv')
 
 df = pd.merge(p, c, left_on='id', right_on='post_id')
-df = df[(df['p_match'].str.len() == 1) & (~df['season'].isin(['2015', '2016', '2017']))]
+df = df[df['p_match'].str.len() == 1]
 df = df.explode('p_match')
 
 df['polarity'] = df['comment_body'].apply(lambda x: analyzer.polarity_scores(x))
 df['comm_sentiment'] = df['polarity'].apply(lambda x: x['compound'])
 
-p_sentiment = df.groupby(['season', 'p_match'])['comm_sentiment'].mean().reset_index()
-p_sentiment = pd.merge(r, p_sentiment, how='left', left_on=['season', 'name'], right_on=['season', 'p_match'])
+sent = df.groupby(['season', 'p_match'])['comm_sentiment'].agg(['mean', 'count']).reset_index()
+sent = pd.merge(r, sent, how='left', left_on=['season', 'name'], right_on=['season', 'p_match'])
 
-sentiment_out = p_sentiment[['season', 'name', 'comm_sentiment', 'returned']]
+sent = sent.rename(columns={'mean': 'comm_sentiment', 'count': 'num_comments'})
+
+sent_out = sent[['season', 'name', 'num_comments', 'comm_sentiment', 'returned']]
+sent_out.to_csv('sentiment_results_full.csv', index=False)
